2b0aebf4ce2bcae5290c6d1ca3dbd2933fd15bbf
YouMeBetween <930798500@qq.com>
2024-11-21 16:14:31 +0800

    从文件中读取项目并随机选择一项

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,43 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
+#include <QFile>
+#include <QRandomGenerator>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QVector<QString> item = getItem();
+    if (item.empty()) {
+        return;
+    }
+    qDebug() << choose(&item) << Qt::endl;
+    return;
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+QVector<QString> MainWindow::getItem()
+{
+    QFile fin(":config/config/cfg.ini");
+    QVector<QString> item;
+    if (!fin.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        qDebug() << tr("Unable to open file.");
+        return item;
+    }
+    while (!fin.atEnd()) {
+        item.push_back(fin.readLine());
+    }
+    fin.close();
+    return item;
+}
+
+QString MainWindow::choose(QVector<QString> *item)
+{
+    return item->at(QRandomGenerator::global()->bounded(item->size()));
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QVector>
+#include <QString>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -19,5 +21,7 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QVector<QString> getItem();
+    QString choose(QVector<QString> *item);
 };
 #endif // MAINWINDOW_H
